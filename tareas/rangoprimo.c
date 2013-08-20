97cb24058e24ffb4a04b123e577c7dd6090ed622
Ricardo <rick_1913aguilazul@hotmail.com>
2013-08-20 03:09:23 -0500

    structprebes

--- tareas/rangoprimo.c
+++ tareas/rangoprimo.c
@@ -6,21 +6,18 @@ int primo(int num);
 
 int main(){
 
-int valor,prm,contprim;
+int valor,prm,contprim,rango;
 
-printf("Hasta que numero deseas conocer los numeros primos que hay en ese rango: ");
+printf("Rango mayor: ");
 scanf("%d",&valor);
 contprim=0;
-prm=primo(valor);
-		if(prm==2)
+rango=primo(valor);
+		if(rango>0)
 		{
-			contprim=contprim+1;
-			printf("Los numeros primos que existen hasta el %d son: %d",valor,numprim);
+			
+			printf("Los numeros primos que existen hasta el %d son: %d",valor,rango);
 		}
-		else
-		{
-			printf("No es un numero primo"); 
-		}
+		
 
 	return 0;
 
@@ -28,14 +25,17 @@ prm=primo(valor);
 }
 
 int primo(int num){
-	int i,cnt;
+	int i,cnt,ran,aux;
 	cnt=0; //variable que cuenta los divisores del número introducido
+	do{
 	for(i=1;i<=num;i++)
 		{
 			if(num%i==0) // si num módulo de i es 0, incrementamos cnt en 1.
 			cnt++;
 		}
-	
-	return cnt;
+	}while(rango<=ran);
+		aux=aux+1;
+		
+	return aux;
 	
 }
